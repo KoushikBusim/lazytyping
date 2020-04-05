5303ebc633b85b94ec2e311125dfce51e4e5915f
KoushikBusim <busimnk@gmail.com>
2020-04-05 14:12:05 +0530

    Updates

---
+++ precache-manifest.148240295123e601a3264de581c9461e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77833643ad6edc9c6427bfcf0f0b362b",
+    "url": "/lazytyping/index.html"
+  },
+  {
+    "revision": "1426f2d27f6f103870d6",
+    "url": "/lazytyping/static/css/main.4daf6744.chunk.css"
+  },
+  {
+    "revision": "5814d090c7f7152f13ed",
+    "url": "/lazytyping/static/js/2.9acd8679.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lazytyping/static/js/2.9acd8679.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1426f2d27f6f103870d6",
+    "url": "/lazytyping/static/js/main.fc60780f.chunk.js"
+  },
+  {
+    "revision": "f834b23f4c886b663cae",
+    "url": "/lazytyping/static/js/runtime-main.873c87a5.js"
+  }
+]);
